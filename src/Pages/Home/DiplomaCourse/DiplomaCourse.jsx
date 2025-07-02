8da5a53467bc9fc62fc9782c3cfd141bc4618120
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-02 15:10:47 +0600

    Diploma Course  🛸

--- src/Pages/Home/DiplomaCourse/DiplomaCourse.jsx
+++ src/Pages/Home/DiplomaCourse/DiplomaCourse.jsx
@@ -8,7 +8,7 @@ import CourseCard from '../../../components/homePage/CourseCard'
 import './DiplomaCourse.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
+// Diploma Course
 const DiplomaCourse = () => {
   // render()
   const settings = {
